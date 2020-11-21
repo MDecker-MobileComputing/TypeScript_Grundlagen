be1bee5d02aad1d07439abace8d4a61f6e274211
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-11-21 16:49:14 +0100

    Windchill-Berechnung: Ergebnis ist jetzt auf eine Nachkommastelle gerundet.

--- beispiele/WindchillBerechnungMitPromise.ts
+++ beispiele/WindchillBerechnungMitPromise.ts
@@ -1,8 +1,7 @@
 
 /*
- *
  * Erklärung zu Promises: https://javascript.info/promise-basics
- *
+ * <br>
  *
  * This file is licensed under the terms of the BSD 3-Clause License.
  */
@@ -40,11 +39,11 @@ class WetterProvider {
    * Funktion zur Simulation einer "langlaufenden" Anfrage, die die aktuelle Windgeschwindigkeit
    * am Ort des Nutzers zurückgibt.
    *
-   * @return Promise mit Windgeschwindigkeit in km/h, Promise löst immer auf 12 km/h auf.
+   * @return Promise mit Windgeschwindigkeit in km/h, Promise löst immer auf 15 km/h auf.
    */
   private async getWindgeschwindigkeit(): Promise<Number> {
 
-    const geschwindigkeitsObjeit = new Number(12);
+    const geschwindigkeitsObjeit = new Number(15);
 
     let promise = new Promise<Number>(function(resolveCallback, rejectCallback) {
 
@@ -64,26 +63,33 @@ class WetterProvider {
    * * https://www.welt.de/wissenschaft/article112099898/Wie-jeder-seine-gefuehlte-Temperatur-ausrechnen-kann.html
    * * https://de.wikipedia.org/wiki/Windchill
    *
-   * @param temperatur  Physikalische Lufttemperatur in Grad Celsius
+   * @param temperatur  Physikalische Lufttemperatur in Grad Celsius.
    *
-   * @param windgeschwindigkeit  Windgeschwindigkeit in km/h
+   * @param windgeschwindigkeit  Windgeschwindigkeit in km/h.
+   *
+   * @return  Gefühlte Temperatur auf eine Nachkommastelle gerundet.
    */
   private berechneGefuehlteTemperatur(temperatur: Number, windgeschwindigkeit: Number) : Number {
 
     const v_a = temperatur.valueOf();
     const v   = Math.pow(windgeschwindigkeit.valueOf(), 0.16);
 
-    return 13.12 + 0.6215*v_a + (0.3965*v_a - 11.37) * v;
+    const gefuehlteTemperatur =  13.12 + 0.6215*v_a + (0.3965*v_a - 11.37) * v;
+
+    const gefuehlteTempraturGerundet = Math.round(gefuehlteTemperatur * 10) / 10;
+
+    return gefuehlteTempraturGerundet;
   }
 
 
   /**
-   * Berechnung der gefühlten Temperatur OHNE await.
+   * Berechnung der gefühlten Temperatur OHNE await: Syntaktisch nicht möglich,
+   * weil die Promise-Objekte nicht aufgelöst werden..
    */
   public gefuehlteTemperatur_1() {
 
     const temperatur = wetterProvider.getTemperatur();
-    console.log(`Temperatur: ${temperatur} ° Celsius`);
+    console.log(`Temperatur: ${temperatur} Grad Celsius`);
 
     const windgeschwindigkeit = wetterProvider.getWindgeschwindigkeit();
     console.log(`Windgeschwindigkeit: ${windgeschwindigkeit} km/h`);
@@ -99,15 +105,14 @@ class WetterProvider {
   public async gefuehlteTemperatur_2() {
 
     const temperatur = await wetterProvider.getTemperatur();
-    console.log(`Temperatur: ${temperatur} ° Celsius`);
+    console.log(`Temperatur: ${temperatur} Grad Celsius`);
 
     const windgeschwindigkeit = await wetterProvider.getWindgeschwindigkeit();
     console.log(`Windgeschwindigkeit: ${windgeschwindigkeit} km/h`);
 
     const gefuehlteTemp = this.berechneGefuehlteTemperatur(temperatur, windgeschwindigkeit);
-    console.log(`Gefühlte Temperatur: ${temperatur} ° Celsius\n`);
+    console.log(`Gefühlte Temperatur: ${gefuehlteTemp} Grad Celsius\n`);
   }
-
 
 }; // Ende class
 
@@ -117,7 +122,7 @@ console.log();
 
 let wetterProvider = new WetterProvider();
 
-wetterProvider.gefuehlteTemperatur_1();
+//wetterProvider.gefuehlteTemperatur_1();
 
 wetterProvider.gefuehlteTemperatur_2();
 
