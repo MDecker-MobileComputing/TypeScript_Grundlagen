9994afc640a9857725ff627da8844aeeeb0506fa
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-11-21 16:40:14 +0100

    Windchill: Promise<number> als Rueckgabetyp.

--- beispiele/WindchillBerechnungMitPromise.ts
+++ beispiele/WindchillBerechnungMitPromise.ts
@@ -3,32 +3,31 @@
  *
  * Erklärung zu Promises: https://javascript.info/promise-basics
  *
- * Erklärung Windchill-Formel:
- * * https://www.welt.de/wissenschaft/article112099898/Wie-jeder-seine-gefuehlte-Temperatur-ausrechnen-kann.html
- * * https://de.wikipedia.org/wiki/Windchill
  *
  * This file is licensed under the terms of the BSD 3-Clause License.
  */
 
- /** "npm install" ausführen */
-//import {Promise} from 'es6-promise';
-
-/* Wartezeit bis Funktion ein Ergebnis zurückgibt. */
+/**
+ * Simulierte Wartezeit, bis eine der Funktionen zum Holen von Temperatur bzw. Windgeschwindigkeit
+ * das Ergebnis zurückgibt.
+ */
 const ZWEI_SEKUNDEN = 2;
 
 class WetterProvider {
 
   /**
-   * Funktion zur Simulation einer "langlaufenden" Anfrage, die die aktuelle Temperatur
+   * Funktion zur Simulation einer "langlaufenden" Anfrage, die die aktuelle Lufttemperatur
    * am Ort des Nutzers zurückgibt.
    *
    * @return Promise mit Temperatur in Grad Celsius, löst immer auf 5° auf.
    */
-  public async getTemperatur() {
+  public async getTemperatur(): Promise<Number> {
 
-    let promise = new Promise(function(resolveCallback, rejectCallback) {
+    const temperaturObjekt = new Number(5);
 
-        setTimeout( () => { resolveCallback( 5 ) },
+    const promise = new Promise<Number>(function(resolveCallback, rejectCallback) {
+
+        setTimeout( () => { resolveCallback( temperaturObjekt ) },
                     ZWEI_SEKUNDEN * 1000
         );
     });
@@ -43,11 +42,13 @@ class WetterProvider {
    *
    * @return Promise mit Windgeschwindigkeit in km/h, Promise löst immer auf 12 km/h auf.
    */
-  public async getWindgeschwindigkeit() {
+  private async getWindgeschwindigkeit(): Promise<Number> {
 
-    let promise = new Promise(function(resolveCallback, rejectCallback) {
+    const geschwindigkeitsObjeit = new Number(12);
 
-        setTimeout( () => { resolveCallback( 12 ) },
+    let promise = new Promise<Number>(function(resolveCallback, rejectCallback) {
+
+        setTimeout( () => { resolveCallback( geschwindigkeitsObjeit ) },
                     ZWEI_SEKUNDEN * 1000
         );
     });
@@ -56,22 +57,55 @@ class WetterProvider {
   }
 
 
-  public berechneGefuehlteTemperatur_1() {
+  /**
+   * Eigentliche Berechnung der gefühlten Temperatur.<br>
+   *
+   * Erklärung Windchill-Formel:
+   * * https://www.welt.de/wissenschaft/article112099898/Wie-jeder-seine-gefuehlte-Temperatur-ausrechnen-kann.html
+   * * https://de.wikipedia.org/wiki/Windchill
+   *
+   * @param temperatur  Physikalische Lufttemperatur in Grad Celsius
+   *
+   * @param windgeschwindigkeit  Windgeschwindigkeit in km/h
+   */
+  private berechneGefuehlteTemperatur(temperatur: Number, windgeschwindigkeit: Number) : Number {
 
-    let windgeschwindigkeit = wetterProvider.getWindgeschwindigkeit();
-    console.log(`Windgeschwindigkeit: ${windgeschwindigkeit} km/h`);
+    const v_a = temperatur.valueOf();
+    const v   = Math.pow(windgeschwindigkeit.valueOf(), 0.16);
 
-    let temperatur = wetterProvider.getTemperatur();
-    console.log(`Temperatur: ${temperatur} ° Celsius\n`);
+    return 13.12 + 0.6215*v_a + (0.3965*v_a - 11.37) * v;
   }
 
-  public async berechneGefuehlteTemperatur_2() {
 
-    let windgeschwindigkeit = await wetterProvider.getWindgeschwindigkeit();
+  /**
+   * Berechnung der gefühlten Temperatur OHNE await.
+   */
+  public gefuehlteTemperatur_1() {
+
+    const temperatur = wetterProvider.getTemperatur();
+    console.log(`Temperatur: ${temperatur} ° Celsius`);
+
+    const windgeschwindigkeit = wetterProvider.getWindgeschwindigkeit();
     console.log(`Windgeschwindigkeit: ${windgeschwindigkeit} km/h`);
 
-    let temperatur = await wetterProvider.getTemperatur();
-    console.log(`Temperatur: ${temperatur} ° Celsius\n`);
+    //const gefuehlteTemp = this.berechneGefuehlteTemperatur(temperatur, windgeschwindigkeit);
+    //console.log(`Gefühlte Temperatur: ${temperatur} ° Celsius\n`);
+  }
+
+
+  /**
+   * Berechnung der gefühlten Temperatur MIT await.
+   */
+  public async gefuehlteTemperatur_2() {
+
+    const temperatur = await wetterProvider.getTemperatur();
+    console.log(`Temperatur: ${temperatur} ° Celsius`);
+
+    const windgeschwindigkeit = await wetterProvider.getWindgeschwindigkeit();
+    console.log(`Windgeschwindigkeit: ${windgeschwindigkeit} km/h`);
+
+    const gefuehlteTemp = this.berechneGefuehlteTemperatur(temperatur, windgeschwindigkeit);
+    console.log(`Gefühlte Temperatur: ${temperatur} ° Celsius\n`);
   }
 
 
@@ -83,9 +117,9 @@ console.log();
 
 let wetterProvider = new WetterProvider();
 
-wetterProvider.berechneGefuehlteTemperatur_1();
+wetterProvider.gefuehlteTemperatur_1();
 
-//wetterProvider.berechneGefuehlteTemperatur_2();
+wetterProvider.gefuehlteTemperatur_2();
 
 console.log();
 
