a799564fd859e0fb6c4d478661f1061a87fef139
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-11-22 10:13:29 +0100

    Verbessert: PromiseMitFehlerfall.ts

--- beispiele/PromiseMitFehlerfall.ts
+++ beispiele/PromiseMitFehlerfall.ts
@@ -1,17 +1,14 @@
+// This file is licensed under the terms of the BSD 3-Clause License.
 
-/*
- * Erklärung zu Promises (Callbacks, Executor): https://javascript.info/promise-basics
- * <br>
- *
- * This file is licensed under the terms of the BSD 3-Clause License.
- */
 
 /**
- * Funktion liefert in 30% der Aufrufe einen Fehler zurück. In Abhängigkeit von einem
- * Zufallsgenerator wird entweder: 
- * * ein Promise-Objekt zurückgeliefert, bei dem nach einer Sekunde die Callback-Methode 
+ * Funktion liefert in 50% der Aufrufe einen Fehler zurück. In Abhängigkeit von einem
+ * Zufallsgenerator wird entweder
+ *
+ * * ein Promise-Objekt zurückgeliefert, bei dem nach einer Sekunde die Callback-Methode
  *   für den Erfolgsfall aufgerufen wird (die Temperatur ist dann immer 5°).
- * * ein Promise-Objekt zurückgeliefert, bei dem nach einer halben Sekunden die Callback-Methode
+ *
+ * * ein Promise-Objekt zurückgeliefert, bei dem nach einer halben Sekunde die Callback-Methode
  *   für den Fehlerfall aufgerufen wird.
  */
 function getTemperatur(): Promise<Number> {
@@ -20,22 +17,22 @@ function getTemperatur(): Promise<Number> {
 
   let promise = null;
 
-  if (zufallszahl <= 0.3) {
+  if (zufallszahl <= 0.5) {
 
     const temperaturObjekt = new Number(5);
 
     promise = new Promise<Number>(function(resolveCallback, rejectCallback) {
-  
+
         setTimeout(
             function() { resolveCallback( temperaturObjekt ); },
             1000
         );
     });
-  
+
   } else {
 
     promise = new Promise<Number>(function(resolveCallback, rejectCallback) {
-  
+
         setTimeout(
             function() { rejectCallback( "Verbindung zu Server mit Wetterdaten fehlgeschlagen." ); },
             500
@@ -50,18 +47,15 @@ function getTemperatur(): Promise<Number> {
 
 // **********************************************************************************************************************************
 
-console.log();
 
 const temperaturPromise = getTemperatur();
+
 temperaturPromise.then( function(temperaturResolved) {
 
-  console.log(`Temperatur: ${temperaturResolved} Grad Celsius`);
+  console.log(`\nTemperatur: ${temperaturResolved} Grad Celsius\n`);
 
 }).catch( function(fehlerObjekt) {
 
-  console.log(`Fehler aufgetreten: ${fehlerObjekt}`);
+  console.log(`\nFehler aufgetreten: ${fehlerObjekt}\n`);
 
 });
-
-console.log();
-
