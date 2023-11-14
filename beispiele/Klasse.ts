86e00793e76064a9fac9f65ffcb9939a20ced199
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-11-14 14:00:08 +0100

    Klasse.ts: Rueckgabetyp ist jetzt "string" statt "String"

--- beispiele/Klasse.ts
+++ beispiele/Klasse.ts
@@ -1,25 +1,25 @@
 
- /* 
+ /*
   * Beispiel für eine Klasse in TypeScript.
   * siehe auch: https://www.typescriptlang.org/docs/handbook/classes.html
   *
   * This file is licensed under the terms of the BSD 3-Clause License.
   */
 
-  
+
 class Begruessung {
 
     /*
      * Wenn das Argument mit einem Schutzmodizierer versehen ist (hier: private),
      * dann wird automatisch eine Member-Variablen mit entsprechendem Typ angelegt.
      */
-    constructor( private _name: string ) {                 
+    constructor( private _name: string ) {
     }
 
     /*
      * Gibt String mit Begrüßungstext zurück.
      */
-    public getBegruessung(): String {
+    public getBegruessung(): string {
 
         return "\nHallo " + this._name + "!\n";
     }
