e00784ef595a6a2f0b719b825d8d195364e6953a
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-19 13:15:07 +0100

    PromiseMitFehlerfall: Methode getTemperatur() verbessert

--- beispiele/promises/PromiseMitFehlerfall.ts
+++ beispiele/promises/PromiseMitFehlerfall.ts
@@ -15,14 +15,14 @@ class PromiseMitFehlerfall {
   * * ein Promise-Objekt zurückgeliefert, bei dem nach einer Sekunde die Callback-Methode
   *   für den Erfolgsfall aufgerufen wird (die Temperatur ist dann immer 5°).
   *
-  * * oder ein Promise-Objekt zurückgeliefert, bei dem nach einer halben Sekunde die 
+  * * oder ein Promise-Objekt zurückgeliefert, bei dem nach einer halben Sekunde die
   *   Callback-Methode für den Fehlerfall aufgerufen wird.
   */
  private static async getTemperatur(): Promise<Number> {
 
     const zufallszahl = Math.random(); // Zufallszahl (gleichverteilt) zwischen 0.0 und 1.0 erzeugen.
 
-    let promise = null;
+    let promise;
 
     if (zufallszahl <= 0.5) {
 
